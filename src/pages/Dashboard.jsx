4e8f1d5241374a96aae2ad2c36fe28f2a41c0676
shami <shamialam8423@gmail.com>
2025-08-23 14:13:46 +0530

    bugs fixed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,15 @@
 import React, { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+// import { AuthContext } from "../context/AuthContext";
 
 export default function Dashboard() {
-  const { logout } = useContext(AuthContext);
+  // const { logout } = useContext(AuthContext);
 
   return (
     <div className="container mt-5">
       <h2>Welcome to Document Management System</h2>
-      <button className="btn btn-danger mt-3" onClick={logout}>
+      {/* <button className="btn btn-danger mt-3" onClick={logout}>
         Logout
-      </button>
+      </button> */}
     </div>
   );
 }
